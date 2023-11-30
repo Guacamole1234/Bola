9f9e9668c36bbd8a3bf818909b2ed495b8cc8af8
Guacamole1234 <thegamerlapalma@gmail.com>
2023-11-30 16:06:12 +0000

    Initial commit

---
+++ Assets/Movimiento.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    private Rigidbody Cuerpo;
+    public float Velocidad;
+    bool Saltando = false;
+    public float FuerzaSalto = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cuerpo = GetComponent<Rigidbody>();
+        Velocidad = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movHorizontal = Input.GetAxis("Horizontal");
+        float movVertical = Input.GetAxis("Vertical");
+        Cuerpo.velocity = new Vector3(movHorizontal * Velocidad, Cuerpo.velocity.y ,movVertical * Velocidad);
+        Saltando = Input.GetButtonDown("Jump");
+
+        if (Saltando)
+        {
+            Cuerpo.AddForce(new Vector3(0, FuerzaSalto, 0), ForceMode.Impulse);
+        }
+    }
+}
